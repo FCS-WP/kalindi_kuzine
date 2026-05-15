f8f4437b43f13419853b0c43537024aa874512d5
Thong Ha Minh <thong.haminh@floatingcube.com>
2026-05-15 11:12:19 +0700

    Stop tracking ignored blocks assets

--- src/wp-content/themes/ai-zippy-child/assets/blocks/banner/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f26af379865f13a7a937');
--- src/wp-content/themes/ai-zippy-child/assets/blocks/page-header/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd71452cfbfce9be590d5');
